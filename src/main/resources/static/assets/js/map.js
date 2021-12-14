eddb732dcd3c2a564156ca6b4b53b46181008a9e
jenkwon92 <silverkay92@gmail.com>
2021-12-14 11:57:34 +0900

    위치기반 구현 js

--- src/main/resources/static/assets/js/map.js
+++ src/main/resources/static/assets/js/map.js
@@ -1,304 +1,64 @@
-"use strict";
+// map 정리 최종본 - 21/12/14
+$(function() {
 
-var map, marker, markerContent, searchClicked;
-var optimizedDatabaseLoading = 0;
+	var mapContainer = document.getElementById('map'), // 지도를 표시할 div
+		mapOption = {
+			center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+			level: 3 // 지도의 확대 레벨
+		};
 
-function heroMap(_latitude,_longitude, element, mapDefaultZoom, mapAutoZoom){
+	// 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
+	var map = new kakao.maps.Map(mapContainer, mapOption);
 
-    if( document.getElementById(element) !== null ){
+	function locationLoadSuccess(pos) {
+		// 현재 위치 받아오기
+		var currentPos = new kakao.maps.LatLng(pos.coords.latitude,  pos.coords.longitude);
 
-        // Create google map first -------------------------------------------------------------------------------------
+		// 지도 이동(기존 위치와 가깝다면 부드럽게 이동)
+		map.panTo(currentPos);
+	};
 
-        if( !mapDefaultZoom ){
-            mapDefaultZoom = 14;
-        }
+	function locationLoadError(pos) {
+		alert('위치 정보를 가져오는데 실패했습니다.');
+	};
 
-        if( !optimizedDatabaseLoading ){
-            optimizedDatabaseLoading = 0;
-        }
+	$("#getMyPositionBtn").on("click", function() {
+		navigator.geolocation.getCurrentPosition(locationLoadSuccess, locationLoadError);
+		// 주소-좌표 변환 객체를 생성합니다
+		var geocoder = new kakao.maps.services.Geocoder();
 
-        map = new google.maps.Map(document.getElementById(element), {
-            zoom: mapDefaultZoom,
-            scrollwheel: false,
-            center: new google.maps.LatLng(_latitude, _longitude),
-            mapTypeId: "roadmap",
-            styles: [{"featureType":"administrative.locality","elementType":"all","stylers":[{"hue":"#c79c60"},{"saturation":7},{"lightness":19},{"visibility":"on"}]},{"featureType":"landscape","elementType":"all","stylers":[{"hue":"#ffffff"},{"saturation":-100},{"lightness":100},{"visibility":"simplified"}]},{"featureType":"poi","elementType":"all","stylers":[{"hue":"#ffffff"},{"saturation":-100},{"lightness":100},{"visibility":"off"}]},{"featureType":"road","elementType":"geometry","stylers":[{"hue":"#c79c60"},{"saturation":-52},{"lightness":-10},{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"hue":"#c79c60"},{"saturation":-93},{"lightness":31},{"visibility":"on"}]},{"featureType":"road.arterial","elementType":"labels","stylers":[{"hue":"#c79c60"},{"saturation":-93},{"lightness":-2},{"visibility":"simplified"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"hue":"#c79c60"},{"saturation":-52},{"lightness":-10},{"visibility":"simplified"}]},{"featureType":"transit","elementType":"all","stylers":[{"hue":"#c79c60"},{"saturation":10},{"lightness":69},{"visibility":"on"}]},{"featureType":"water","elementType":"all","stylers":[{"hue":"#c79c60"},{"saturation":-78},{"lightness":67},{"visibility":"simplified"}]}]
-        });
+		// 중심 좌표나 확대 수준이 변경됐을 때 지도 중심 좌표에 대한 주소 정보를 표시하도록 이벤트를 등록합니다
+		kakao.maps.event.addListener(map, 'idle', function() {
 
-        // Load necessary data for markers using PHP (from database) after map is loaded and ready ---------------------
+			// 현재 지도 중심좌표로 주소를 검색해서 지도 좌측 상단에 표시합니다
+			searchAddrFromCoords(map.getCenter(), displayCenterInfo);
+		});
 
-        var allMarkers;
+		function searchAddrFromCoords(coords, callback) {
+			// 좌표로 행정동 주소 정보를 요청합니다
+			geocoder.coord2RegionCode(coords.getLng(), coords.getLat(), callback);
+		}
 
-        //  When optimization is enabled, map will load the data in Map Bounds every time when it's moved. Otherwise will load data at once
+		function searchDetailAddrFromCoords(coords, callback) {
+			// 좌표로 법정동 상세 주소 정보를 요청합니다
+			geocoder.coord2Address(coords.getLng(), coords.getLat(), callback);
+		}
 
-        if( optimizedDatabaseLoading === 1 ){
-            google.maps.event.addListener(map, 'idle', function(){
-                if( searchClicked !== 1 ){
-                    var ajaxData = {
-                        optimized_loading: 1,
-                        north_east_lat: map.getBounds().getNorthEast().lat(),
-                        north_east_lng: map.getBounds().getNorthEast().lng(),
-                        south_west_lat: map.getBounds().getSouthWest().lat(),
-                        south_west_lng: map.getBounds().getSouthWest().lng()
-                    };
-                    if( markerCluster !== undefined ){
-                        markerCluster.clearMarkers();
-                    }
-                    loadData("assets/php/data.php", newMarkers, ajaxData);
-                }
-            });
-        }
-        else {
-            google.maps.event.addListenerOnce(map, 'idle', function(){
-                loadData("assets/php/data.php", newMarkers);
-            });
-        }
+		//지도 중심좌표에 대한 주소정보를 표출하는 함수입니다
+		function displayCenterInfo(result, status) {
+			if (status === kakao.maps.services.Status.OK) {
+				var login_address = document.getElementById("login_address");
 
-        // Create and place markers function ---------------------------------------------------------------------------
+				for (var i = 0; i < result.length; i++) {
+					// 행정동의 region_type 값은 'H' 이므로
+					if (result[i].region_type === 'H') {
+						login_address.value = result[i].address_name;
+						break;
+					}
+				}
+			}
+		}
+	});
 
-        var i;
-        var a;
-        var newMarkers = [];
-        var resultsArray = [];
-        var visibleMarkersId = [];
-        var visibleMarkersOnMap = [];
-        var markerCluster;
-
-
-
-        $('.geo-location').on("click", function(e) {
-            e.preventDefault();
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(success);
-            } else {
-                console.log('Geo Location is not supported');
-            }
-        });
-
-        // Automatic Geo Location
-
-        if( automaticGeoLocation === true ){
-            navigator.geolocation.getCurrentPosition(success);
-        }
-
-        // Autocomplete
-
-        autoComplete(map,marker);
-
-    }
-    else {
-        //console.log("No map element");
-    }
-
-}
-
-function placeMarkers(markers){
-
-    var newMarkers = [];
-
-    for ( var i = 0; i < markers.length; i++) {
-
-        var marker;
-        var markerContent = document.createElement('div');
-        var thumbnailImage;
-
-        if( markers[i]["marker_image"] !== undefined ){
-            thumbnailImage = markers[i]["marker_image"];
-        }
-        else {
-            thumbnailImage = "assets/img/items/default.png";
-        }
-
-        markerContent.innerHTML =
-            '<div class="marker" data-id="'+ markers[i]["id"] +'">' +
-            '<img src="assets/img/map-marker.png" alt="">' +
-            '</div>';
-
-        // Latitude, Longitude and Address
-
-        if ( markers[i]["latitude"] && markers[i]["longitude"] && markers[i]["address"] ){
-            renderRichMarker(i,"latitudeLongitude");
-        }
-
-        // Only Address
-
-        else if ( markers[i]["address"] && !markers[i]["latitude"] && !markers[i]["longitude"] ){
-            renderRichMarker(i,"address");
-        }
-
-        // Only Latitude and Longitude
-
-        else if ( markers[i]["latitude"] && markers[i]["longitude"] && !markers[i]["address"] ) {
-            renderRichMarker(i,"latitudeLongitude");
-        }
-
-        // No coordinates
-
-        else {
-            console.log( "No location coordinates");
-        }
-    }
-
-    // Create marker using RichMarker plugin -------------------------------------------------------------------
-
-    function renderRichMarker(i,method){
-        if( method === "latitudeLongitude" ){
-            marker = new RichMarker({
-                position: new google.maps.LatLng( markers[i]["latitude"], markers[i]["longitude"] ),
-                map: map,
-                draggable: false,
-                content: markerContent,
-                flat: true
-            });
-
-            google.maps.event.addListener(marker, 'click', (function(marker, i) {
-                return function() {
-                    openInfobox( $(this.content.firstChild).attr("data-id"), this, i );
-                    //console.log( $(this.content.firstChild).attr("data-id") );
-                }
-            })(marker, i));
-            newMarkers.push(marker);
-
-        }
-        else if ( method === "address" ){
-            var a = i;
-            var geocoder = new google.maps.Geocoder();
-            var geoOptions = {
-                address: markers[i]["address"]
-            };
-            geocoder.geocode(geoOptions, geocodeCallback(markerContent));
-
-        }
-
-        if ( mapAutoZoom === 1 ){
-            var bounds  = new google.maps.LatLngBounds();
-            for (var i = 0; i < newMarkers.length; i++ ) {
-                bounds.extend(newMarkers[i].getPosition());
-            }
-            map.fitBounds(bounds);
-        }
-
-    }
-
-    // Ajax loading of infobox -------------------------------------------------------------------------------------
-
-    var lastInfobox;
-
-    function openInfobox(id, _this, i){
-        $.ajax({
-            url: "assets/php/infobox.php",
-            dataType: "html",
-            data: { id: id },
-            method: "POST",
-            success: function(results){
-                var infoboxOptions = {
-                    content: results,
-                    disableAutoPan: false,
-                    pixelOffset: new google.maps.Size(-125, -30),
-                    zIndex: null,
-                    alignBottom: true,
-                    boxClass: "infobox-wrapper",
-                    enableEventPropagation: true,
-                    closeBoxMargin: "0px 0px -8px 0px",
-                    closeBoxURL: "assets/img/close-btn.png",
-                    infoBoxClearance: new google.maps.Size(1, 1)
-                };
-
-                if( lastInfobox !== undefined ){
-                    lastInfobox.close();
-                }
-
-                newMarkers[i].infobox = new InfoBox(infoboxOptions);
-                newMarkers[i].infobox.open(map, _this);
-                lastInfobox = newMarkers[i].infobox;
-
-                setTimeout(function(){
-                    $(".item.infobox[data-id="+ id +"]").parent().addClass("show");
-                }, 10);
-
-                google.maps.event.addListener(newMarkers[i].infobox,'closeclick',function(){
-                    $(lastClickedMarker).removeClass("active");
-                });
-            },
-            error : function () {
-                console.log("error");
-            }
-        });
-    }
-
-    // Geocoder callback ---------------------------------------------------------------------------------------
-
-    function geocodeCallback(markerContent) {
-        return function(results, status) {
-            if (status === google.maps.GeocoderStatus.OK) {
-                marker = new RichMarker({
-                    position: results[0].geometry.location,
-                    map: map,
-                    draggable: false,
-                    content: markerContent,
-                    flat: true
-                });
-                newMarkers.push(marker);
-                if ( mapAutoZoom === 1 ){
-                    var bounds  = new google.maps.LatLngBounds();
-                    for (var i = 0; i < newMarkers.length; i++ ) {
-                        bounds.extend(newMarkers[i].getPosition());
-                    }
-                    map.fitBounds(bounds);
-                }
-                google.maps.event.addListener(marker, 'click', (function(marker, i) {
-                    return function() {
-                        openInfobox( $(this.content.firstChild).attr("data-id"), this, 0 );
-                    }
-                })(marker, i));
-            } else {
-                console.log("Geocode failed " + status);
-            }
-        }
-    }
-
-    // Marker clusters -----------------------------------------------------------------------------------------
-
-    var clusterStyles = [
-        {
-            url: 'assets/img/cluster.png',
-            height: 36,
-            width: 36
-        }
-    ];
-
-    var markerCluster = new MarkerClusterer(map, newMarkers, { styles: clusterStyles, maxZoom: 16, ignoreHidden: true });
-
-}
-
-function loadData(url, newMarkers, ajaxData){
-    $.ajax({
-        url: url,
-        dataType: "json",
-        method: "POST",
-        data: ajaxData,
-        cache: false,
-        success: function(results){
-            for( var i=0; i < newMarkers.length; i++ ){
-                newMarkers[i].setMap(null);
-            }
-            var allMarkers = results;
-            placeMarkers(results);
-        },
-        error : function (e) {
-            console.log(e);
-        }
-    });
-}
-
-// Geo Location ------------------------------------------------------------------------------------------------
-
-function success(position) {
-    var center = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-    map.panTo(center);
-    marker = new google.maps.Marker({
-        position: center,
-        map: map,
-        icon: "assets/img/map-marker.png"
-    });
-}
+	$("#getMyPositionBtn").trigger("click");
+})
