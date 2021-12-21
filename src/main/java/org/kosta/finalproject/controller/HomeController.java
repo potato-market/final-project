df1c003d94b178e22bc3e873da6d39937963e353
anngdung2 <jhsoar1018@gmail.com>
2021-12-22 04:02:50 +0900

    회원 관련 컨트롤러 업로드

--- src/main/java/org/kosta/finalproject/controller/HomeController.java
+++ src/main/java/org/kosta/finalproject/controller/HomeController.java
@@ -1,19 +1,41 @@
 package org.kosta.finalproject.controller;
 
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-	@RequestMapping("/")
-	public String index() {
+	@RequestMapping(value={"/","home"})
+	public String index(Authentication a) {
+		System.out.println("homecont1 "+a);
 		return "home";
 	}
 	
-	@RequestMapping("main")
+	@RequestMapping("/main")
 	public String home() {
+		System.out.println("homeCont2");
 		return "main.tiles";
 	}
+	@RequestMapping("login_success")
+	public String login_success() {
+		System.out.println("login_success");
+		return "user/login_success";
+	}
+	/*
+	 * @RequestMapping("accessDeniedView") public String accessDeniedView() { return
+	 * "auth/accessDeniedView"; }
+	 */
 	
+	@Bean
+	public ApplicationRunner applicationRunner() {
+		return args ->{
+			System.out.println("start application runner ");
+		 
+		};
+		
 	
+	}
 }
