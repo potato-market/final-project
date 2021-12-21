fd30bd56a0e8661e8b9f3aa24895cf90a0c0a445
anngdung2 <jhsoar1018@gmail.com>
2021-12-22 04:04:46 +0900

    회원 VO, authority(VO) 업로드

--- src/main/java/org/kosta/finalproject/model/domain/UserVO.java
+++ src/main/java/org/kosta/finalproject/model/domain/UserVO.java
@@ -1,39 +1,38 @@
 package org.kosta.finalproject.model.domain;
 
-import java.io.Serializable;
-
-
-public class UserVO implements Serializable{
-	private static final long serialVersionUID = -4185469606236616588L;
-
+public class UserVO {
 	private String userId;
+	private String userPassword;
 	private String userTel;
 	private String userAddress;
-	private String userCreatedAt;
-	private String userImage;
-	private String userType;
-	private String userPassword;
 	private String userEmail;
+	private String userImage;
+	private String userEnabled;
+	private String userCreatedAt;
 	public UserVO() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	
-	public UserVO(String userId) {
+	public UserVO(String userId, String userPassword, String userTel, String userAddress, String userEmail,
+			String userImage, String userEnabled, String userCreatedAt) {
 		super();
 		this.userId = userId;
-	}
-
-	public UserVO(String userId, String userTel, String userAddress, String userCreatedAt, String userImage,
-			String userType, String userPassword, String userEmail) {
-		super();
-		this.userId = userId;
+		this.userPassword = userPassword;
 		this.userTel = userTel;
 		this.userAddress = userAddress;
-		this.userCreatedAt = userCreatedAt;
+		this.userEmail = userEmail;
 		this.userImage = userImage;
-		this.userType = userType;
+		this.userEnabled = userEnabled;
+		this.userCreatedAt = userCreatedAt;
+	}
+	
+	
+	public UserVO(String userId, String userPassword, String userTel, String userAddress, String userEmail) {
+		super();
+		this.userId = userId;
 		this.userPassword = userPassword;
+		this.userTel = userTel;
+		this.userAddress = userAddress;
 		this.userEmail = userEmail;
 	}
 	public String getUserId() {
@@ -41,6 +40,12 @@ public class UserVO implements Serializable{
 	}
 	public void setUserId(String userId) {
 		this.userId = userId;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
 	}
 	public String getUserTel() {
 		return userTel;
@@ -54,11 +59,11 @@ public class UserVO implements Serializable{
 	public void setUserAddress(String userAddress) {
 		this.userAddress = userAddress;
 	}
-	public String getUserCreatedAt() {
-		return userCreatedAt;
+	public String getUserEmail() {
+		return userEmail;
 	}
-	public void setUserCreatedAt(String userCreatedAt) {
-		this.userCreatedAt = userCreatedAt;
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
 	}
 	public String getUserImage() {
 		return userImage;
@@ -66,32 +71,24 @@ public class UserVO implements Serializable{
 	public void setUserImage(String userImage) {
 		this.userImage = userImage;
 	}
-	public String getUserType() {
-		return userType;
+	public String getUserEnabled() {
+		return userEnabled;
 	}
-	public void setUserType(String userType) {
-		this.userType = userType;
+	public void setUserEnabled(String userEnabled) {
+		this.userEnabled = userEnabled;
 	}
-	public String getUserPassword() {
-		return userPassword;
+	public String getUserCreatedAt() {
+		return userCreatedAt;
 	}
-	public void setUserPassword(String userPassword) {
-		this.userPassword = userPassword;
-	}
-	public String getUserEmail() {
-		return userEmail;
-	}
-	public void setUserEmail(String userEmail) {
-		this.userEmail = userEmail;
-	}
-	public static long getSerialversionuid() {
-		return serialVersionUID;
+	public void setUserCreatedAt(String userCreatedAt) {
+		this.userCreatedAt = userCreatedAt;
 	}
 	@Override
 	public String toString() {
-		return "UserVO [userId=" + userId + ", userTel=" + userTel + ", userAddress=" + userAddress + ", userCreatedAt="
-				+ userCreatedAt + ", userImage=" + userImage + ", userType=" + userType + ", userPassword="
-				+ userPassword + ", userEmail=" + userEmail + "]";
+		return "UserVO [userId=" + userId + ", userPassword=" + userPassword + ", userTel=" + userTel + ", userAddress="
+				+ userAddress + ", userEmail=" + userEmail + ", userImage=" + userImage + ", userEnabled=" + userEnabled
+				+ ", userCreatedAt=" + userCreatedAt + "]";
 	}
-	
 }
+	
+	
