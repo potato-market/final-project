4bfbf4f470a065486b4d6827767a19d4907e934b
cecyrang <cecilia21c@naver.com>
2021-12-24 02:31:56 +0900

    중고물품 메인화면에서 리스트 불러오기 기능 구현 완료

--- src/main/java/org/kosta/finalproject/controller/HomeController.java
+++ src/main/java/org/kosta/finalproject/controller/HomeController.java
@@ -1,13 +1,22 @@
 package org.kosta.finalproject.controller;
 
+import java.util.List;
+
+import org.kosta.finalproject.model.domain.ItemVO;
+import org.kosta.finalproject.model.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+	
 @Controller
 public class HomeController {
+	@Autowired
+	private ItemService itemService;
+	
 	@RequestMapping(value={"/","home"})
 	public String index(Authentication a) {
 		System.out.println("homecont1 "+a);
@@ -15,8 +24,10 @@ public class HomeController {
 	}
 	
 	@RequestMapping("main")
-	public String home() {
-		System.out.println("homeCont2");
+	public String home(Model model) {
+		List<ItemVO> list = itemService.getAllItemList();
+		System.out.println(list);
+		model.addAttribute("itemList",list);
 		return "main.tiles";
 	}
 	@RequestMapping("login_success")
