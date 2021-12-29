2dec04765c6f181ea5809d66d817cbe5deab6e2e
jenkwon92 <silverkay92@gmail.com>
2021-12-29 14:28:31 +0900

    POTATO-93

--- src/main/java/org/kosta/finalproject/controller/CommunityController.java
+++ src/main/java/org/kosta/finalproject/controller/CommunityController.java
@@ -1,6 +1,8 @@
 package org.kosta.finalproject.controller;
 
+import org.kosta.finalproject.model.domain.CommentVO;
 import org.kosta.finalproject.model.domain.CommunityVO;
+import org.kosta.finalproject.model.service.CommentService;
 import org.kosta.finalproject.model.service.CommunityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,12 +10,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class CommunityController {
 	@Autowired
 	private CommunityService communityService;
-
+	@Autowired
+	private CommentService commentService;
+	
 	@RequestMapping("communityBoard")
 	public String communityList(Model model) {
 		// 전체 글 목록 불러오기
@@ -21,18 +26,12 @@ public class CommunityController {
 		
 		return "community/community-board.tiles";
 	}
-
-	@RequestMapping("communityDetail")
-	public String communityDetail(Model model,int communityId) {
-		model.addAttribute("communityData", communityService.getCommunityById(communityId));
-		return "community/community-detail.tiles";
-	}
-
+	
 	@RequestMapping("communityWriteForm")
 	public String communityWriteForm() {
 		return "community/community-write-form.tiles";
 	}
-
+	
 	@PostMapping("writeCommunity")
 	public String writeCommunity(CommunityVO communityVO) {
 		communityService.writeCommunity(communityVO);
@@ -56,4 +55,30 @@ public class CommunityController {
 		communityService.deleteCommunity(communityId);
 		return "redirect:communityBoard";
 	}
+	
+	@GetMapping("communityDetail")
+	public String communityDetail(Model model,int communityId) {
+		model.addAttribute("communityData",communityService.getCommunityById(communityId));
+		model.addAttribute("commemtList",commentService.getCommentList(communityId));
+		return "community/community-detail.tiles";
+	}
+	
+	
+	@PostMapping("writeComment")
+	@ResponseBody
+	public CommentVO writeComment(CommentVO commentVO) {
+		commentService.writeComment(commentVO);
+		return commentService.findCommentByCommentId(commentVO.getCommentId());
+	}	
+	
+	@PostMapping("deleteComment")
+	@ResponseBody
+	public void deleteComment(int commentId) {
+		commentService.deleteComment(commentId);
+	}
+	
+
+	
+	
+	
 }
