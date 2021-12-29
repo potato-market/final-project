bc30df23d03fc5da65aae385d5f3c1c6f09d5dc3
leetingman <dwa08@naver.com>
2021-12-29 13:45:24 +0800

    채팅 수정

--- src/main/java/org/kosta/finalproject/config/WSConfig.java
+++ src/main/java/org/kosta/finalproject/config/WSConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WsConfig implements WebSocketMessageBrokerConfigurer{
+public class WSConfig implements WebSocketMessageBrokerConfigurer{
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
